2f62af42766870fae7865c3677eda590940a5f20
monicakodidala <monicakodidala@gmail.com>
2016-12-01 19:25:56 -0500

    Added code to display 3 recently added items to the collection and redirection to collectiondetails page on click

--- Comp229-TeamProject/Home.aspx.cs
+++ Comp229-TeamProject/Home.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +12,24 @@ namespace Comp229_TeamProject
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            //Gets 3 recently added items from the collections table
+            SqlConnection connection = new SqlConnection("Server = MONICA\\MONICASQLEXPRESS;Database = Comp229TeamProject;Integrated Security=True");
+            SqlCommand comm = new SqlCommand("Select top 3 Title,CollectionID,ImageURL from Collections order by DateAdded DESC", connection);
+            try
+            {
+                //Binds the data
+                connection.Open();
+                SqlDataReader reader = comm.ExecuteReader();
 
+                Books1.DataSource = reader;
+                Books1.DataBind();
+
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
--- Comp229-TeamProject/Home.aspx.designer.cs
+++ Comp229-TeamProject/Home.aspx.designer.cs
@@ -47,5 +47,14 @@ namespace Comp229_TeamProject {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox txtsearch;
+        
+        /// <summary>
+        /// Books1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater Books1;
     }
 }
